9865d0dbb6c2ed47bd8afb9785d7a289c9b9613b
YANNICK LERNER <47801170@ort>
2024-06-06 10:41:04 -0300

    gfa

---
+++ app/nuevaVista/nuevaVista.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+const NuevaVista = () => {
+  return (
+    <div>
+      <h1>¡Bienvenido a la Nueva Vista!</h1>
+      
+    </div>
+  );
+};
+
+export default NuevaVista;
